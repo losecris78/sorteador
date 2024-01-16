b46c2784ca5a24c107361b4cfad7fd6ded2c102a
Cristian Lopez <losecris@gmail.com>
2024-01-16 13:01:11 -0300

    Commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+const { parseInt } = require("lodash");
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('form-sorteador').addEventListener('submit',function(event){
+        event.preventDefault();
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo= parseInt(numeroMaximo);
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        numeroAleatorio = Math.round(numeroAleatorio);
+
+        document.getElementById('numero-sorteado').innerText= numeroAleatorio;
+           })
+})
